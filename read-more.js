f3fbcd2476b5e7c2426db33f2c3dbd7246652ee3
Wilson <wilsontu4979@gmail.com>
2021-07-04 18:08:21 -0700

    hi

---
+++ read-more.js
@@ -0,0 +1,20 @@
+
+function readMore(){
+    var readMore = document.getElementById("read-more-btn");
+    var moreText = document.getElementById("more-text");
+    var dots = document.getElementById("dots");
+
+    if(dots.style.display === "none"){
+        dots.style.display = "inline";
+        moreText.style.display = "none";
+        readMore.innerHTML = "Read More";
+
+    } else{
+        dots.style.display = "none";
+        moreText.style.display = "inline";
+        readMore.innerHTML = "Read Less";
+    }
+
+
+}
+
